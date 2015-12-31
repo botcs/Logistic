0cea4b566ae0dd792b409cd50a5f902a4c1b52ca
botcs <botos.csaba@hotmail.hu>
2015-12-31 02:34:12 +0100

    mess

--- main.cpp
+++ main.cpp
@@ -2,23 +2,21 @@
 
 using namespace std;
 
-#define VERBOSE
-
 #include "instance.h"
 #include "randomGen.h"
 
-#define FringeHeurestics
 #include <ctime>
 int main()
 {
-    ofstream OP("parancsok.txt");
+    ofstream OP("parancsok.txt"), LOG("lepesek.txt");
     try{
-        //generate(1000, 1000);
+        generate(300, 300);
         InstanceHandler inst;
+        inst.LogType(InstanceHandler::showAll);
         auto time = clock();
         //inst.loadData("large_test_map.txt", "large_test_container.txt");
         //inst.loadData("small_test_map.txt", "small_test_container.txt");
-        //inst.loadData("test_ship.txt", "test_cont.txt");
+        //inst.loadData("test_ship.txt", "test_cargo.txt");
         inst.loadData("menetrend.txt", "rakomany.txt");
         cout << "FILE LOADING FINISHED IN " << clock() - time << " miliseconds\n\n";
 
@@ -26,11 +24,11 @@ int main()
 
         time = clock();
         inst.solveAll(true);
+
         cout << "\nSOLUTION GIVEN IN " << clock() - time << " miliseconds\n"
              << "\nTOTAL CITY LOOKUPS: " << inst.getStepCount() << endl;
         inst.printOperations(OP);
-
-        inst.printContainers(cout);
+        inst.printLog(LOG);
     }catch (exception& e)
     {
         cout << e.what() << '\n';
